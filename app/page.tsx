0a2d3b30d9805040ae867187be637a32dbd69373
Andrews Frempong <frempongandrews@yahoo.com>
2024-11-30 11:48:39 +0000

    logo ticker section done

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
 											src="/images/message.png"
 											className="hidden sm:inline absolute -right-[0px] md:-right-[40px]"
 										/>
-										<h1 className="lg:text-red-600 text-6xl sm:text-7xl md:text-8xl font-bold tracking-tighter text-center leading-[70px]">
+										<h1 className="text-6xl sm:text-7xl md:text-8xl font-bold tracking-tighter text-center leading-[70px]">
 											One Task <br />
 											at a Time
 										</h1>
@@ -111,6 +111,35 @@ export default function Home() {
 						</div>
 						{/* end hero */}
 					</div>
+					{/* logo ticker */}
+					<div className="bg-black text-white py-[72px] sm:py-24">
+						<div>
+							<h2 className="text-xl text-center text-white/70">
+								Trusted by the world&apos;s most innovative teams
+							</h2>
+							<div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative before:bg-red-500 after:bg-red-500 after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0))]">
+								<div className="flex gap-16">
+									<img
+										src="/images/logos/acme-logo.png"
+										alt="acme logo"
+										className="flex-none h-8 w-auto"
+									/>
+									<img src="/images/logos/apex-logo.png" alt="apex logo" />
+									<img
+										src="/images/logos/celestia-logo.png"
+										alt="celestia logo"
+									/>
+									<img src="/images/logos/echo-logo.png" alt="echo logo" />
+									<img src="/images/logos/pulse-logo.png" alt="pulse logo" />
+									<img
+										src="/images/logos/quantum-logo.png"
+										alt="quantum logo"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* End logo ticker */}
 				</main>
 				<footer>footer</footer>
 			</div>
