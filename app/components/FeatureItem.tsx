4a78848d1308f6a391a7e197336b46808dd3288e
Andrews Frempong <frempongandrews@yahoo.com>
2024-12-01 22:34:12 +0000

    animations in progress...

--- app/components/FeatureItem.tsx
+++ app/components/FeatureItem.tsx
@@ -1,14 +1,45 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
 type FeatureItemProps = {
 	title: string;
 	description: string;
 };
 
 export const FeatureItem = ({ title, description }: FeatureItemProps) => {
+	const borderRef = useRef<HTMLDivElement>(null);
+	const updateMousePosition = (e: MouseEvent) => {
+		if (!borderRef.current) return;
+
+		const borderRefRect = borderRef.current?.getBoundingClientRect();
+		const offsetX = e.x - borderRefRect.x;
+		const offsetY = e.x - borderRefRect.y;
+	};
+	useEffect(() => {
+		window.addEventListener("mousemove", updateMousePosition);
+
+		// cleanup
+		return () => {
+			window.removeEventListener("mousemove", updateMousePosition);
+		};
+	}, []);
 	return (
 		<div
 			key={title}
-			className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1"
+			className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative"
 		>
+			{/* glowing border */}
+			<div
+				ref={borderRef}
+				className="absolute inset-0 border-2 rounded-xl border-purple-400"
+				style={{
+					WebkitMaskImage:
+						"radial-gradient(100px 100px at 0px 0px, black, transparent)",
+					maskImage:
+						"radial-gradient(100px 100px at 0px 0px, black, transparent)",
+				}}
+			></div>
 			<div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
 				<img src="/images/ecosystem-icon.png" />
 			</div>
