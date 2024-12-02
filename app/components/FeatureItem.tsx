1005fbff85d30e8fc9ab2967fd6209e20d32678c
Andrews Frempong <frempongandrews@yahoo.com>
2024-12-02 17:49:22 +0000

    animations in progress...

--- app/components/FeatureItem.tsx
+++ app/components/FeatureItem.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import {
+	motion,
+	motionValue,
+	useMotionTemplate,
+	useMotionValue,
+} from "framer-motion";
 
 type FeatureItemProps = {
 	title: string;
@@ -9,12 +15,15 @@ type FeatureItemProps = {
 
 export const FeatureItem = ({ title, description }: FeatureItemProps) => {
 	const borderRef = useRef<HTMLDivElement>(null);
+	const offsetX = useMotionValue(0);
+	const offsetY = useMotionValue(0);
+	const maskImage = useMotionTemplate`radial-gradient(100px 100px at ${offsetX}px ${offsetY}px, black, transparent)`;
 	const updateMousePosition = (e: MouseEvent) => {
 		if (!borderRef.current) return;
 
 		const borderRefRect = borderRef.current?.getBoundingClientRect();
-		const offsetX = e.x - borderRefRect.x;
-		const offsetY = e.x - borderRefRect.y;
+		offsetX.set(e.x - borderRefRect.x);
+		offsetY.set(e.y - borderRefRect.y);
 	};
 	useEffect(() => {
 		window.addEventListener("mousemove", updateMousePosition);
@@ -30,16 +39,14 @@ export const FeatureItem = ({ title, description }: FeatureItemProps) => {
 			className="border border-white/30 px-5 py-10 text-center rounded-xl sm:flex-1 relative"
 		>
 			{/* glowing border */}
-			<div
+			<motion.div
 				ref={borderRef}
 				className="absolute inset-0 border-2 rounded-xl border-purple-400"
 				style={{
-					WebkitMaskImage:
-						"radial-gradient(100px 100px at 0px 0px, black, transparent)",
-					maskImage:
-						"radial-gradient(100px 100px at 0px 0px, black, transparent)",
+					WebkitMaskImage: maskImage,
+					maskImage,
 				}}
-			></div>
+			></motion.div>
 			<div className="inline-flex h-14 w-14 bg-white text-black justify-center items-center rounded-lg">
 				<img src="/images/ecosystem-icon.png" />
 			</div>
